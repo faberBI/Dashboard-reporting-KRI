8a62c3fb2ac3f3d66f618af1e5fa3e328ddb7fcc
Fabrizio <fabriziodisciorio91@gmail.com>
2025-10-10 09:13:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -334,7 +334,7 @@ if selected_kri == "⚡ Energy Risk":
         df_ebitda=df,  # qui passi il DataFrame df con la colonna "Ebitda"
         font_path="utils/TIMSans-Medium.ttf"    
             )
-        st.pyplot(fig_var, dpi=80)
+        st.pyplot(fig_var, dpi=160)
 
         # Salvataggio in session_state
         st.session_state.update({
