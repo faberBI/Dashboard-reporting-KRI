96e9daf415f52ae08cf2fe47b26432688212ab1b
Fabrizio <fabriziodisciorio91@gmail.com>
2025-12-12 15:42:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -350,8 +350,9 @@ if selected_kri == "⚡ Energy Risk":
         df_hist_styled = df_historical.style.format({"Historical Price": format_euro}).background_gradient(cmap='Greens', low=0.1, high=0.4)
         st.session_state.df_historical = df_historical
         st.dataframe(df_hist_styled)
-
-        predict_price = forecast_price['50%'].values.tolist()
+        
+        predict_price = yearly_means.values.tolist()
+        # predict_price = forecast_price['50%'].values.tolist()
         p95 = forecast_price['95%'].values.tolist()
         p5 = forecast_price['5%'].values.tolist()
 
