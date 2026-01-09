71c33ce6c1d6cd04d22ddcddecf185026f4d07a1
Fabrizio <fabriziodisciorio91@gmail.com>
2026-01-09 14:47:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -329,31 +329,28 @@ if selected_kri == "⚡ Energy Risk":
         df_sim["Year"] = df_sim.index.year
 
         # Percentili annuali
-        annual_lower_emp = df_sim.groupby("Year").apply(lambda x: np.percentile(x.iloc[:, :-1].values.flatten(), 2.5))
-        annual_upper_emp = df_sim.groupby("Year").apply(lambda x: np.percentile(x.iloc[:, :-1].values.flatten(), 97.5))
+        annual_lower_emp = (df_sim.groupby("Year").apply(lambda x: np.percentile(x.iloc[:, :-1].values.flatten(), 2.5)))
+
+        annual_upper_emp = (df_sim.groupby("Year").apply(lambda x: np.percentile(x.iloc[:, :-1].values.flatten(), 97.5)))
+
 
         # Conformal calibration annuale
-        annual_calibration_y = prezzi_storici_df.resample("Y").mean().values[-len(annual_lower_emp):]
-
-        samples_cal = np.random.choice(simulated_prices.flatten(), size=(len(annual_lower_emp), simulated_prices.shape[0]))
-
+        hist_annual = (prezzi_storici_df.resample("Y").mean().tail(len(annual_lower_emp)).values)
+        samples_cal = np.random.choice(simulated_prices.flatten(),size=(len(hist_annual), simulated_prices.shape[0]),replace=True)
         lower_cal = np.percentile(samples_cal, 2.5, axis=1)
         upper_cal = np.percentile(samples_cal, 97.5, axis=1)
+        nonconformity = np.maximum(lower_cal - hist_annual,hist_annual - upper_cal)
 
-        nonconformity = np.maximum(lower_cal - annual_calibration_y, annual_calibration_y - upper_cal)
         q_hat = np.quantile(np.append(nonconformity, np.inf), 0.95)
 
-        # Intervalli conformal annuali
-        lower_adj_annual = annual_lower_emp - q_hat
-        upper_adj_annual = annual_upper_emp + q_hat
+        annual_interval_df = pd.DataFrame(
+        {"Lower_Conformal": annual_lower_emp.values - q_hat,"Upper_Conformal": annual_upper_emp.values + q_hat,}, index=annual_lower_emp.index)
 
-        # DataFrame finale annuale
-        annual_interval_df = pd.DataFrame({
-        "Lower_Conformal": lower_adj_annual,
-        "Upper_Conformal": upper_adj_annual
-        }, index=annual_lower_emp.index)
+        annual_interval_df.index.name = "Year"
 
-        st.table(annual_interval_df)
+        st.subheader("📊 Intervalli conformali annuali")
+        st.dataframe(annual_interval_df)
+
 
         
         
