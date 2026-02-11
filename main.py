3c890ab79355aa76788667f9cf47b5b2884bd534
Fabrizio <fabriziodisciorio91@gmail.com>
2026-02-11 17:14:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ if selected_kri == "⚡ Energy Risk":
         hist_pun = pd.read_excel(data_path)
         hist_pun["log_return"] = np.log(hist_pun["GMEPIT24 Index"] / hist_pun["GMEPIT24 Index"].shift(1))
         hist_pun["Month"] = hist_pun["Date"].dt.month
-        series = hist_pun[hist_pun['Year']>year]
+        series = hist_pun[hist_pun['Year']>2015]
         PUN_monthly_forecast = forecast_monthly_prices(series, n_years=n_year)
         st.success("✅ Modello ibrido allenato!")
         st.subheader("📈 Prezzo PUN e Volatilità")
