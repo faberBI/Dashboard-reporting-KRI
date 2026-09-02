c097affd555bc69fb52be9ccd78d7bce7b3be630
Fabrizio <fabriziodisciorio91@gmail.com>
2026-09-02 12:47:22 +0200

    Adjust volatility weighting in price simulation

--- functions/energy_risk.py
+++ functions/energy_risk.py
@@ -109,7 +109,7 @@ def simulate_prices(PUN_monthly_forecast, PUN_monthly, monthly_sigma, monthly_st
     np.random.seed(seed)
     vol_h = monthly_sigma * np.array(PUN_monthly)
     vol_m = np.array(monthly_std['std_log_return_montly'].values) * np.array(PUN_monthly)
-    vol_f = vol_h*0.30+ vol_m*0.7
+    vol_f = vol_h*0.40+ vol_m*0.6
     
     n_total_months = len(PUN_monthly_forecast)
     n_years = n_total_months // 12
